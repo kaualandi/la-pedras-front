4f91915e0c82d0048c0ec9ed81a655e93326dd13
Kauã Landi <eu@kaualf.com>
2023-02-16 21:44:32 -0300

    build: fix prod error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { DatePipe, registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import {
-  DEFAULT_CURRENCY_CODE,
-  isDevMode,
-  LOCALE_ID,
-  NgModule,
+  DEFAULT_CURRENCY_CODE, LOCALE_ID,
+  NgModule
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
@@ -13,6 +11,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgxMaskModule } from 'ngx-mask';
+import { environment } from './../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -38,7 +37,7 @@ registerLocaleData(localePt);
     MatInputModule,
     NgxMaskModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
