67fac0bbdf8e4a67b3427bc55b430d937184b228
Guilherme Santana <guilherme.santana1998@gmail.com>
2023-02-24 18:27:24 +0000

    feat: update keep token cookie to 60 days

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -14,14 +14,14 @@ export class StorageService {
   /**
    * Função para setar o token no cookie
    * @param token Token que vem da API
-   * @param keep Se true, o cookie expira em 365 dias, se false, o cookie expira quando o browser é fechado
+   * @param keep Se true, o cookie expira em 60 dias, se false, o cookie expira quando o browser é fechado
    * @returns void
    */
   setToken(token: string, keep = false): void {
     this.cookieService.set(
       'token',
       token,
-      keep ? 365 : undefined,
+      keep ? 60 : undefined,
       '/',
       undefined,
       true,
