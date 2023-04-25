f848ba382fc242d155387d88972dbdfd9e8895d4
Kauã Landi <eu@kaualf.com>
2023-04-25 19:50:34 -0300

    first commit to la-pedras

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-boilerplate'),
+      dir: require('path').join(__dirname, './coverage/la-pedras'),
       subdir: '.',
       reporters: [
         { type: 'html' },
